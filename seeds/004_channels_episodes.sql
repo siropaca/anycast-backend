f7d7086a1e679828f7147a09f91c74bbd7eb7d22
siropaca <ryhshr3@gmail.com>
2026-01-20 23:44:58 +0900

    fix: seed ファイルから line_type カラムを削除

--- seeds/004_channels_episodes.sql
+++ seeds/004_channels_episodes.sql
@@ -86,39 +86,39 @@ INSERT INTO episodes (id, channel_id, title, description, user_prompt, published
 -- ===========================================
 
 -- Episode 1: AI の未来を語る
-INSERT INTO script_lines (id, episode_id, line_order, line_type, speaker_id, text, emotion) VALUES
-	('236f9071-900a-4b75-aea7-ebb847f5ccad', 'eb960304-f86e-4364-be5d-d3d5126c9601', 0, 'speech', 'd1f7e3e3-d2e9-4a8f-a155-99b1e3ccf007', 'こんにちは、テックトークへようこそ！今日は AI の未来について話していきます。', NULL),
-	('bff9d166-1ad5-46fa-96fb-39a27378e99f', 'eb960304-f86e-4364-be5d-d3d5126c9601', 1, 'speech', '4cee85f3-adec-4333-84e6-d6aaefb63408', 'よろしくお願いします！最近 ChatGPT がすごく話題ですよね。', NULL),
-	('d5422671-73c8-4b28-afe1-5b0c419dcd49', 'eb960304-f86e-4364-be5d-d3d5126c9601', 2, 'speech', 'd1f7e3e3-d2e9-4a8f-a155-99b1e3ccf007', 'そうなんです。大規模言語モデルの進化は目覚ましいものがあります。', NULL),
-	('51223f66-3ac5-4685-9609-50d0ccd9b10a', 'eb960304-f86e-4364-be5d-d3d5126c9601', 3, 'speech', '4cee85f3-adec-4333-84e6-d6aaefb63408', 'いつか AI がポッドキャストを作る時代が来るかもしれませんね！', '笑いながら');
+INSERT INTO script_lines (id, episode_id, line_order, speaker_id, text, emotion) VALUES
+	('236f9071-900a-4b75-aea7-ebb847f5ccad', 'eb960304-f86e-4364-be5d-d3d5126c9601', 0, 'd1f7e3e3-d2e9-4a8f-a155-99b1e3ccf007', 'こんにちは、テックトークへようこそ！今日は AI の未来について話していきます。', NULL),
+	('bff9d166-1ad5-46fa-96fb-39a27378e99f', 'eb960304-f86e-4364-be5d-d3d5126c9601', 1, '4cee85f3-adec-4333-84e6-d6aaefb63408', 'よろしくお願いします！最近 ChatGPT がすごく話題ですよね。', NULL),
+	('d5422671-73c8-4b28-afe1-5b0c419dcd49', 'eb960304-f86e-4364-be5d-d3d5126c9601', 2, 'd1f7e3e3-d2e9-4a8f-a155-99b1e3ccf007', 'そうなんです。大規模言語モデルの進化は目覚ましいものがあります。', NULL),
+	('51223f66-3ac5-4685-9609-50d0ccd9b10a', 'eb960304-f86e-4364-be5d-d3d5126c9601', 3, '4cee85f3-adec-4333-84e6-d6aaefb63408', 'いつか AI がポッドキャストを作る時代が来るかもしれませんね！', '笑いながら');
 
 -- Episode 2: スマートホームのすすめ
-INSERT INTO script_lines (id, episode_id, line_order, line_type, speaker_id, text, emotion) VALUES
-	('d8909d91-da04-4ec6-bec1-356eb9c4e2d9', '67e8e26d-20c8-492a-ac2c-5c79d8050aa3', 0, 'speech', 'd1f7e3e3-d2e9-4a8f-a155-99b1e3ccf007', '今日はスマートホームについて紹介していきます。', NULL),
-	('f0d41215-6172-4bca-a10e-efaa002a09fc', '67e8e26d-20c8-492a-ac2c-5c79d8050aa3', 1, 'speech', '4cee85f3-adec-4333-84e6-d6aaefb63408', 'スマートホームって難しそうなイメージがあるんですけど', NULL),
-	('6723d570-d7c6-4a07-b481-0b609765be86', '67e8e26d-20c8-492a-ac2c-5c79d8050aa3', 2, 'speech', 'd1f7e3e3-d2e9-4a8f-a155-99b1e3ccf007', '私も最初は全然分からなかったんですよ。でもやってみたら意外と簡単でした。', '笑いながら'),
-	('504ebb03-a05c-49ce-9e94-974f9cc80cc0', '67e8e26d-20c8-492a-ac2c-5c79d8050aa3', 3, 'speech', '4cee85f3-adec-4333-84e6-d6aaefb63408', 'なるほど！それなら私でもできそうです。', NULL);
+INSERT INTO script_lines (id, episode_id, line_order, speaker_id, text, emotion) VALUES
+	('d8909d91-da04-4ec6-bec1-356eb9c4e2d9', '67e8e26d-20c8-492a-ac2c-5c79d8050aa3', 0, 'd1f7e3e3-d2e9-4a8f-a155-99b1e3ccf007', '今日はスマートホームについて紹介していきます。', NULL),
+	('f0d41215-6172-4bca-a10e-efaa002a09fc', '67e8e26d-20c8-492a-ac2c-5c79d8050aa3', 1, '4cee85f3-adec-4333-84e6-d6aaefb63408', 'スマートホームって難しそうなイメージがあるんですけど', NULL),
+	('6723d570-d7c6-4a07-b481-0b609765be86', '67e8e26d-20c8-492a-ac2c-5c79d8050aa3', 2, 'd1f7e3e3-d2e9-4a8f-a155-99b1e3ccf007', '私も最初は全然分からなかったんですよ。でもやってみたら意外と簡単でした。', '笑いながら'),
+	('504ebb03-a05c-49ce-9e94-974f9cc80cc0', '67e8e26d-20c8-492a-ac2c-5c79d8050aa3', 3, '4cee85f3-adec-4333-84e6-d6aaefb63408', 'なるほど！それなら私でもできそうです。', NULL);
 
 -- Episode 3: 最近ハマってること
-INSERT INTO script_lines (id, episode_id, line_order, line_type, speaker_id, text, emotion) VALUES
-	('23e48682-4949-4751-aef0-b80e369a899b', '198d7e19-7d40-4299-95bf-a641f5c83911', 0, 'speech', 'b0b67254-ff3b-4b5e-96fa-073ce5c8a6a4', 'ねえねえ、最近なんかハマってることある？', NULL),
-	('8b7f30af-1662-413d-84b6-2b27033435f7', '198d7e19-7d40-4299-95bf-a641f5c83911', 1, 'speech', '41977119-13d8-4d26-bfe4-694eb2cf2167', '最近はコーヒーにハマってるかな。豆から挽いて淹れてるよ。', NULL),
-	('fd9ce404-74c8-456e-8f1b-eda25e22ccce', '198d7e19-7d40-4299-95bf-a641f5c83911', 2, 'speech', 'b0b67254-ff3b-4b5e-96fa-073ce5c8a6a4', 'すごい凝ってるね！私なんてインスタントで十分だよ。', '笑いながら'),
-	('18641a58-561f-466a-a670-cf3a569c6669', '198d7e19-7d40-4299-95bf-a641f5c83911', 3, 'speech', '41977119-13d8-4d26-bfe4-694eb2cf2167', 'インスタントも美味しいよね。手軽さって大事。', NULL);
+INSERT INTO script_lines (id, episode_id, line_order, speaker_id, text, emotion) VALUES
+	('23e48682-4949-4751-aef0-b80e369a899b', '198d7e19-7d40-4299-95bf-a641f5c83911', 0, 'b0b67254-ff3b-4b5e-96fa-073ce5c8a6a4', 'ねえねえ、最近なんかハマってることある？', NULL),
+	('8b7f30af-1662-413d-84b6-2b27033435f7', '198d7e19-7d40-4299-95bf-a641f5c83911', 1, '41977119-13d8-4d26-bfe4-694eb2cf2167', '最近はコーヒーにハマってるかな。豆から挽いて淹れてるよ。', NULL),
+	('fd9ce404-74c8-456e-8f1b-eda25e22ccce', '198d7e19-7d40-4299-95bf-a641f5c83911', 2, 'b0b67254-ff3b-4b5e-96fa-073ce5c8a6a4', 'すごい凝ってるね！私なんてインスタントで十分だよ。', '笑いながら'),
+	('18641a58-561f-466a-a670-cf3a569c6669', '198d7e19-7d40-4299-95bf-a641f5c83911', 3, '41977119-13d8-4d26-bfe4-694eb2cf2167', 'インスタントも美味しいよね。手軽さって大事。', NULL);
 
 -- Episode 4: 副業から始める起業入門（test_user2）
-INSERT INTO script_lines (id, episode_id, line_order, line_type, speaker_id, text, emotion) VALUES
-	('ae5f21f0-a737-47cf-8d00-e9f490bea753', 'fcb16526-951a-4ff1-a456-ab1dba96f699', 0, 'speech', 'a4e0f973-f91a-4103-b758-fed371622046', '今日は副業から起業を始める方法についてお話しします。', NULL),
-	('1a87b77a-2211-4421-9f2c-334ce913e5c3', 'fcb16526-951a-4ff1-a456-ab1dba96f699', 1, 'speech', 'b7efbbae-0655-46f1-afb7-a42d2646f0c1', '私も最初は副業からスタートしました。いきなり会社を辞めるのはリスクが高いですからね。', NULL),
-	('b8f262e5-f027-484a-a0f5-997e5b9dd569', 'fcb16526-951a-4ff1-a456-ab1dba96f699', 2, 'speech', 'a4e0f973-f91a-4103-b758-fed371622046', '私なんて最初の副業で赤字出しちゃいましたからね。今となっては良い思い出ですけど。', '笑いながら'),
-	('da61ccde-dfea-4ad8-8a84-f8c4d5a79ac3', 'fcb16526-951a-4ff1-a456-ab1dba96f699', 3, 'speech', 'b7efbbae-0655-46f1-afb7-a42d2646f0c1', 'そうですね。失敗しても学びになりますし、挑戦することが大切です。', NULL);
+INSERT INTO script_lines (id, episode_id, line_order, speaker_id, text, emotion) VALUES
+	('ae5f21f0-a737-47cf-8d00-e9f490bea753', 'fcb16526-951a-4ff1-a456-ab1dba96f699', 0, 'a4e0f973-f91a-4103-b758-fed371622046', '今日は副業から起業を始める方法についてお話しします。', NULL),
+	('1a87b77a-2211-4421-9f2c-334ce913e5c3', 'fcb16526-951a-4ff1-a456-ab1dba96f699', 1, 'b7efbbae-0655-46f1-afb7-a42d2646f0c1', '私も最初は副業からスタートしました。いきなり会社を辞めるのはリスクが高いですからね。', NULL),
+	('b8f262e5-f027-484a-a0f5-997e5b9dd569', 'fcb16526-951a-4ff1-a456-ab1dba96f699', 2, 'a4e0f973-f91a-4103-b758-fed371622046', '私なんて最初の副業で赤字出しちゃいましたからね。今となっては良い思い出ですけど。', '笑いながら'),
+	('da61ccde-dfea-4ad8-8a84-f8c4d5a79ac3', 'fcb16526-951a-4ff1-a456-ab1dba96f699', 3, 'b7efbbae-0655-46f1-afb7-a42d2646f0c1', 'そうですね。失敗しても学びになりますし、挑戦することが大切です。', NULL);
 
 -- Episode 5: 失敗しない資金調達の秘訣（test_user2）
-INSERT INTO script_lines (id, episode_id, line_order, line_type, speaker_id, text, emotion) VALUES
-	('089e59a2-e26b-4dcc-aeca-6763a7ab16b9', '9cde2abb-30e8-447b-bc8b-bb799b0f6f06', 0, 'speech', 'a4e0f973-f91a-4103-b758-fed371622046', '今回は資金調達について詳しくお話ししていきます。', NULL),
-	('83fccc88-5647-47df-9757-5ebd19b301c7', '9cde2abb-30e8-447b-bc8b-bb799b0f6f06', 1, 'speech', 'b7efbbae-0655-46f1-afb7-a42d2646f0c1', '資金調達って種類がたくさんありますよね。VC、エンジェル投資家、融資', NULL),
-	('8f197ebe-677f-4d9d-add7-111af58b6c04', '9cde2abb-30e8-447b-bc8b-bb799b0f6f06', 2, 'speech', 'a4e0f973-f91a-4103-b758-fed371622046', 'その通りです。事業のフェーズによって最適な調達方法は変わってきます。', NULL),
-	('2fecb517-5d29-4f74-b3a7-7a85700e4e22', '9cde2abb-30e8-447b-bc8b-bb799b0f6f06', 3, 'speech', 'b7efbbae-0655-46f1-afb7-a42d2646f0c1', '私も最初のピッチでは緊張しすぎて、投資家の名前を間違えちゃいました。', '笑いながら');
+INSERT INTO script_lines (id, episode_id, line_order, speaker_id, text, emotion) VALUES
+	('089e59a2-e26b-4dcc-aeca-6763a7ab16b9', '9cde2abb-30e8-447b-bc8b-bb799b0f6f06', 0, 'a4e0f973-f91a-4103-b758-fed371622046', '今回は資金調達について詳しくお話ししていきます。', NULL),
+	('83fccc88-5647-47df-9757-5ebd19b301c7', '9cde2abb-30e8-447b-bc8b-bb799b0f6f06', 1, 'b7efbbae-0655-46f1-afb7-a42d2646f0c1', '資金調達って種類がたくさんありますよね。VC、エンジェル投資家、融資', NULL),
+	('8f197ebe-677f-4d9d-add7-111af58b6c04', '9cde2abb-30e8-447b-bc8b-bb799b0f6f06', 2, 'a4e0f973-f91a-4103-b758-fed371622046', 'その通りです。事業のフェーズによって最適な調達方法は変わってきます。', NULL),
+	('2fecb517-5d29-4f74-b3a7-7a85700e4e22', '9cde2abb-30e8-447b-bc8b-bb799b0f6f06', 3, 'b7efbbae-0655-46f1-afb7-a42d2646f0c1', '私も最初のピッチでは緊張しすぎて、投資家の名前を間違えちゃいました。', '笑いながら');
 
 -- ===========================================
 -- お気に入り・ブックマーク・フォロー
